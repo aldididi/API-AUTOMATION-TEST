a8d5dceb83e6d836564f7de22797cf87a3a12bda
aldididi <aldyh024@gmail.com>
2025-10-24 17:03:40 +0700

    add cases on createUser and add README

---
+++ README.md
@@ -0,0 +1,56 @@
+## API Automation Dummy Project
+
+This project provides automated tests for the [https://reqres.in/#try-api] API using Java, TestNG, RestAssured.
+
+### Prerequisites
+
+- Java 17 and above
+- Maven
+- Internet connection 
+
+### Project Structure
+
+```
+src/
+  main/
+    java/
+        org.example/
+            base/
+                basetest
+            utils/
+                configreader
+                testdata 
+  test/
+     java/
+        apiengine/
+        payloads/
+        tests/
+    resources/
+      applicationss.properties/
+```
+
+### How to Run
+
+1. **Clone the repository:**
+   ```sh
+   git https://github.com/aldididi/API-AUTOMATION-TEST.git
+   cd API-AUTOMATION-TEST
+   ```
+
+2. **install dependencies:**
+      ```sh
+      mvn clean install
+      ```
+
+3. **Run the tests:**
+    - With Maven:
+      ```sh
+      mvn test
+      ```
+##
+
+##
+List of test cases:
+https://docs.google.com/spreadsheets/d/1OhZOs9d93FmugPWdrQoYfBxvzRxzRDdfjkReI4q4J-g/edit?usp=sharing
+
+##
--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.example.utils.TestData;
 import org.testng.annotations.BeforeClass;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import payloads.Payload;
 
@@ -35,7 +36,30 @@ public class CreateUserTest {
                 "0:00"
         );
     }
+    private String buildUserPayload2(String email, String gender) {
+        return Payload.createUser(
+                "mr", "adudu", "dudududu",
+                "https://randomuser.me/api/portraits/med/men/74.jpg",
+                gender, email,
+                "1963-04-12T15:41:39.051Z",
+                "76120280",
+                "6839, Lavendelvej",
+                "Agerbæk",
+                "Sjælland",
+                "Denmark",
+                "0:00"
+        );
+    }
 
+    @DataProvider(name="genderProvider")
+    public Object[][] getGender() {
+        return new Object[][] {
+                {"male", false},
+                {"female", false},
+                {null, true},
+                {"unknown", true}
+        };
+    }
 
     @Test
     public void createUser() {
@@ -57,9 +81,31 @@ public class CreateUserTest {
         Assert.assertEquals(response.getStatusCode(), STATUS_BAD_REQUEST,"400 expected, email already user");
         response.prettyPrint();
     }
+
+    @Test(dependsOnMethods = {"createUser"}, dataProvider = "genderProvider")
+    public void createUserWithGender(String gender, boolean expectBadRequest) {
+
+        String randomEmail = "user_" + UUID.randomUUID().toString().substring(0,8) + "@mail.com";
+        String reqBody = buildUserPayload2(randomEmail, gender);
+
+        System.out.println("gender: " + gender);
+        Response response = collectionAPI.createUser(reqBody);
+
+        int expectedStatus = expectBadRequest ? STATUS_BAD_REQUEST : STATUS_OK;
+        Assert.assertEquals(response.getStatusCode(), expectedStatus,"gender is mismatch: "+gender);
+        response.prettyPrint();
+    }
+
     @Test(dependsOnMethods = {"createUser"})
     public void createUserMissingEmail() {
         String reqBody = buildUserPayload(null);
+        Response response = collectionAPI.createUser(reqBody);
+        Assert.assertEquals(response.getStatusCode(), STATUS_BAD_REQUEST,"400 expected, email is missing");
+        response.prettyPrint();
+    }
+    @Test(dependsOnMethods = {"createUser"})
+    public void createUserWrongEmailFormat() {
+        String reqBody = buildUserPayload("email.com");
         Response response = collectionAPI.createUser(reqBody);
         Assert.assertEquals(response.getStatusCode(), STATUS_BAD_REQUEST,"400 expected, email is missing");
         response.prettyPrint();
